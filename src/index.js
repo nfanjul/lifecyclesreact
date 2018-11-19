ffdeaa775dcc55b2f9286a8f5ac69b67d74eca46
Marcela Ramirez <marceram87@gmail.com>
2018-11-19 14:14:25 +0100

    Adding commit logo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Log, VisualizerProvider } from 'react-lifecycle-visualizer';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Log, VisualizerProvider } from "react-lifecycle-visualizer";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import commit from "./images/commit.png";
 
-ReactDOM.render(<VisualizerProvider>
-    <div style={{ display: 'flex' }}>
-        <App />
-        <Log />
+ReactDOM.render(
+  <VisualizerProvider>
+    <div style={{ display: "flex",  position: "relative" }}>
+      <App />
+      <Log />
+      <div className="commit-logo">
+        <img alt="Commit Logo" src={commit} />
+      </div>
     </div>
-</VisualizerProvider>
-    , document.getElementById('root'));
+  </VisualizerProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
