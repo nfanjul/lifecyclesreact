c520e91698f86250c8f6859ebca30c39c268537b
javierferbau <javierferb@gmail.com>
2019-01-09 09:08:46 +0100

    change coments

--- src/components/HomeButton/HomeButton.jsx
+++ src/components/HomeButton/HomeButton.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'; // useEffect es un "substitutivo" de los componentDidXxx
 
 function HomeButton(props) {
-    // inicializa una variable (count) y una funcion que va a setear esa variable (setCount)
-    // esto seria lo equivalente que hacer un this.state = { count } y this.setState({ count: var}) en un componente clase
+    // inicializa una variable (name) y una funcion que va a setear esa variable (setName)
+    // esto seria lo equivalente que hacer un this.state = { name } y this.setState({ name: var}) en un componente clase
     const [buttonName, setName] = useState('Enter god mode');
 
     useEffect(()=>{ // una vez updateado el componente comprobamos si estamos en Home y seteamos con el hook el nombre segun el caso
